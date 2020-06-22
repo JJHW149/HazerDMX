cca2f37114bc7fbfda0b07fa0ebdd6f8ec99276a
Joshua Wright <jjhwrocks@hotmail.com>
2020-06-23 00:49:09 +0930

    Functional and Tested code + Schematic

--- HazerDMX.ino
+++ HazerDMX.ino
@@ -3,13 +3,14 @@
 
 // Constant Variables
 const int relayPin = 2;
-const int addrDMX = 254;
+const int addrDMX = 512;
 
 // Setup pins
 void setup() {
   DMXSerial.init(DMXReceiver);
   
   pinMode(relayPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   DMXSerial.write(addrDMX, 0);
   digitalWrite(relayPin, LOW);
 }
@@ -21,10 +22,13 @@ void loop() {
   if(lastPacket < 10000) {
     if(DMXSerial.read(addrDMX) <= 85){
       digitalWrite(relayPin, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
     } else {
       digitalWrite(relayPin, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH);
     }
   } else {
     digitalWrite(relayPin, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
